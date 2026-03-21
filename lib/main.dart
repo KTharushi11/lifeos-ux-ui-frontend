6296b2e4a0009d9002ae8f3147f83022d6c32d33
KTharushi11 <tharushinimsara702@gmail.com>
2026-03-21 15:41:48 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
       ),
 
       home: const MyHomePage(title: 'LifeOS Home'),
